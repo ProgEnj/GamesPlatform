77301c282e296a8e8e7815284ef083973005a0fd
Dmytro <web11rider@gmail.com>
2026-07-04 22:09:16 +0300

    style: remove space

--- Backend/GamesPlatform.Infrastructure/Extentions/ConfigureExtensions.cs
+++ Backend/GamesPlatform.Infrastructure/Extentions/ConfigureExtensions.cs
@@ -21,7 +21,6 @@ public static class ConfigureExtension
             })
             .AddRoles<IdentityRole>()
             .AddEntityFrameworkStores<ApplicationDbContext>()
-            
             .AddDefaultTokenProviders();
     }
 
